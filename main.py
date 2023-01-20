d1c8d5e8bea47d75ef0acfb541a6b98b56dd092f
KevinXD <kevinfdkord@gmail.com>
2023-01-20 21:55:54 +0200

    fixed bugs and impoved code style

--- main.py
+++ main.py
@@ -1,49 +1,51 @@
 import time
 
-import numpy
-import pandas as pd
 import asyncio
 import aiohttp
+from aiohttp import ClientSession, ClientResponse, ClientPayloadError
 from PIL import Image, UnidentifiedImageError
+import pandas as pd
 import io
-
-from aiohttp import ClientPayloadError
 
 file_data_frame = pd.read_excel("Parser_ImageSize.xlsx")
 amount_of_links = file_data_frame.index.stop
 
 
-async def get_size(response, index):
+async def get_size(response: ClientResponse):
     try:
         data = await response.read()
         buff = io.BytesIO(data)
         size = Image.open(buff).size
         return f"{size[0]}x{size[1]}"
-    except (UnidentifiedImageError, ClientPayloadError):
-        file_data_frame.loc[index]["SIZE"] = "cannot identify image"
+    except UnidentifiedImageError as e:
+        print(f"{e}; image not found")
 
 
-async def store_size(session, link, index):
+async def store_size(session: ClientSession, link: str, index: int):
     try:
         async with session.get(url=link) as response:
-            size = await get_size(response, index)
+            size = await get_size(response)
             file_data_frame.loc[index]["SIZE"] = size
-    except TypeError:
-        file_data_frame.loc[index]["SIZE"] = numpy.NaN
+    except TypeError as e:
+        print(f"{e}; URL is not valid")
 
 
 async def main():
-    async with aiohttp.ClientSession() as session:
-        return await asyncio.gather(
-            *[store_size(session=session,
-                         link=file_data_frame.loc[index]["image_url"],
-                         index=index
-                         ) for index in range(0, amount_of_links-1)]
-        )
+    try:
+        async with aiohttp.ClientSession() as session:
+            return await asyncio.gather(
+                *[store_size(session=session,
+                             link=file_data_frame.loc[index]["image_url"],
+                             index=index
+                             ) for index in range(0, amount_of_links)]
+            )
+    except ClientPayloadError as e:
+        print(e)
+
 
 if __name__ == '__main__':
     start = time.perf_counter()
     asyncio.run(main())
-    file_data_frame.to_excel("Parser_ImageSize2.xlsx")
+    file_data_frame.to_excel("Parser_ImageSize2.xlsx", sheet_name="Solution")
     end = time.perf_counter()
     print("Elapsed", end - start)
